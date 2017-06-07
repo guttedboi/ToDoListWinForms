261ccf3fec84e25beaeefab53dd166f1655a6efe
guttedboi <chovder.gm@gmail.com>
2017-06-07 13:37:17 +0500

    Add files via upload

---
+++ workspace.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoListWinForms
+{
+    public partial class workspace : Form
+    {
+        public workspace()
+        {
+            InitializeComponent();
+
+            timer.Start();
+        }
+
+        private void workspace_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            this.timeLabel.Text = dateTime.ToString();
+        }
+
+    }
+}
