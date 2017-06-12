f48001013ef1cfc32f01eedcc380786297f42197
guttedboi <chovder.gm@gmail.com>
2017-06-12 18:03:16 +0500

    upd

--- Program.cs
+++ Program.cs
@@ -27,42 +27,43 @@ namespace ToDoListWinForms
 
         public static void Auth()
         {
+            auth authForm = new auth();
+            workspace workSpace = new workspace();
+
+            MySqlConnection myConn;
+            string myConnString;
+            myConnString = "server=141.8.195.82;uid=a0141773_todolis;" +
+                "pwd=apache12;database=a0141773_todolist;";
+
             try
             {
-               
-                auth authForm = new auth();
-                MySqlConnection myConn = new MySqlConnection("server=141.8.195.82;port=3306;username=a0141773_todolis;database=a0141773_todolist;password=apache12");
+                myConn = new MySqlConnection(myConnString);
                 myConn.Open();
-                MySqlCommand comm = new MySqlCommand("select user, password from users where user = '" + authForm.loginBox.Text + "' and password = '" + authForm.pwdBox.Text + "';", myConn);
-                //MySqlDataAdapter myDataAdapter = new MySqlDataAdapter();
-                //myDataAdapter.SelectCommand = comm.ExecuteReader;
-                //MySqlCommandBuilder commBuilder = new MySqlCommandBuilder(myDataAdapter);
-                MySqlDataReader reader;
-                reader = comm.ExecuteReader();
-
-
+                MySqlCommand comm = new MySqlCommand("SELECT * FROM a0141773_todolist.users WHERE user = '" + authForm.loginBox.Text + "' and password = '" + authForm.pwdBox.Text + "';", myConn);
+                MySqlDataReader myReader;
+                myReader = comm.ExecuteReader();
                 int count = 0;
-                while (reader.Read())   
+                while (myReader.Read())
                 {
                     count = count + 1;
                 }
-                if (count == 0)
+                if (count == 1)
                 {
 
+                    authForm.Hide();
                     authForm.Close();
-                    workspace workSpace = new workspace();
                     workSpace.ShowDialog();
                 }
                 else
                 {
                     authForm.auth_error.Visible = true;
                 }
-                myConn.Close();
+
             }
-            catch (Exception ex)
+            catch (MySqlException ex)
             {
                 MessageBox.Show(ex.Message);
-            }
+            }         
         }        
         public static void getGroup()
         {
